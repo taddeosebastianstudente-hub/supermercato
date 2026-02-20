a1be0a07e168513c87d298c77521a400dfe6f3a3
taddeosebastianstudente-hub <taddeo.sebastian.studente@itispaleocapa.it>
2026-02-20 10:34:04 +0100

    Add files via upload
    funzioni

---
+++ project3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+struct prodotto{
+	string nome;
+	float prezzo;
+	string categoria;
+	};
+void aggiungi(prodotto p,string p[],int d){	
+	}
+void visualizza(prodotto p[], int d){
+	}
+void cerca(string p[], int d, string p){
+    }
+bool cancella(string p[], int d, string p){
+    }
+bool modifica(string p[], int d, string p){
+    }
+int main () {
+	prodotto p;
+	prodotto supermercato[100];
+	return 0;
+}
